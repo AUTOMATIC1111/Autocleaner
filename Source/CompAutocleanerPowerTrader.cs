31c529f51883f032c44c0ef79d631299034d0de6
AUTOMATIC <16777216c@gmail.com>
2020-04-07 19:35:57 +0300

    cleaner body def fixed potential fix for power transmitter error

--- Source/CompAutocleanerPowerTrader.cs
+++ Source/CompAutocleanerPowerTrader.cs
@@ -4,20 +4,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Verse;
 
 namespace Autocleaner
 {
-    class CompAutocleanerPowerTrader : CompPowerTrader
+    public class CompAutocleanerPowerTrader : CompPowerTrader
     {
 
         public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+           
+        }
+
+        public override void PostDeSpawn(Map map)
+        {
+            map.powerNetManager.Notify_ConnectorDespawned(this);
+            map.mapDrawer.MapMeshDirty(parent.Position, MapMeshFlag.PowerGrid, true, false);
+        }
+        public override void LostConnectParent()
         {
 
         }
 
         public override void PostDraw()
         {
-
+            
         }
+
     }
 }
